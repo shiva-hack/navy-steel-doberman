869b23001bcfec554c483c01721bb832a8db2682
Shivam Dhruva <email@shivamdhruva.com>
2023-12-04 20:29:41 +0530

    remove unused rake script from the render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -4,7 +4,6 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec rake css:build
 bundle exec rails db:migrate
 
 #if you have seeds to run add:
